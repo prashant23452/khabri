22f67b9e6030a21d3b0fbdc1fb156d5369692d68
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-07-12 12:28:30 +0530

    27

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,60 +3,60 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 
 export class News extends Component {
-  articles =[
-    {
-      "source": {
-          "id": "abc-news-au",
-          "name": "ABC News (AU)"
-      },
-      "author": "ABC News",
-      "title": "Indian Prime Minister Narendra Modi meets Vladimir Putin in Russia, condemns deaths of children in war and calls for peace",
-      "description": "Narendra Modi's pointed remarks came just moments after he was welcomed to the Kremlin by Vladimir Putin.",
-      "url": "https://www.abc.net.au/news/2024-07-10/narendra-modi-discusses-ukraine-with-vladimir-putin-in-russia/104077902",
-      "urlToImage": "https://live-production.wcms.abc-cdn.net.au/28f964e516f1663c43caa3836fd21f49?impolicy=wcms_watermark_news&cropH=2074&cropW=3687&xPos=0&yPos=39&width=862&height=485&imformat=generic",
-      "publishedAt": "2024-07-09T19:09:50Z",
-      "content": "Indian Prime Minister Narendra Modi has told Russian President Vladimir Putin that the death of innocent children was painful and terrifying, a day after a lethal strike on Kyiv's main hospital for c… [+5158 chars]"
-  },
-  {
-      "source": {
-          "id": "google-news-in",
-          "name": "Google News (India)"
-      },
-      "author": "The Times of India",
-      "title": "Mumbai BMW hit-and-run case: Juhu bar's license suspended for serving liquor to 23-year-old accused Mihir - The Times of India",
-      "description": "The state excise administration has suspended the license of a Juhu-Tara Road bar that served hard liquor to Mihir Shah, who had not yet attained the age of 25 years. This is a basic criterion for serving or drinking",
-      "url": "https://news.google.com/rss/articles/CBMisAFodHRwczovL3RpbWVzb2ZpbmRpYS5pbmRpYXRpbWVzLmNvbS9pbmRpYS9tdW1iYWktYm13LWhpdC1hbmQtcnVuLWNhc2UtanVodS1iYXJzLWxpY2Vuc2Utc3VzcGVuZGVkLWZvci1zZXJ2aW5nLWxpcXVvci10by0yMy15ZWFyLW9sZC1hY2N1c2VkLW1paGlyLXNoYWgvYXJ0aWNsZXNob3cvMTExNjEzNzgwLmNtc9IBtAFodHRwczovL3RpbWVzb2ZpbmRpYS5pbmRpYXRpbWVzLmNvbS9pbmRpYS9tdW1iYWktYm13LWhpdC1hbmQtcnVuLWNhc2UtanVodS1iYXJzLWxpY2Vuc2Utc3VzcGVuZGVkLWZvci1zZXJ2aW5nLWxpcXVvci10by0yMy15ZWFyLW9sZC1hY2N1c2VkLW1paGlyLXNoYWgvYW1wX2FydGljbGVzaG93LzExMTYxMzc4MC5jbXM?oc=5",
-      "urlToImage": "https://miller-ogorchock.com/wp-content/uploads/2016/10/hit-and-run.jpg",
-      "publishedAt": "2024-07-09T17:06:00+00:00",
-      "content": null
-  },
-  {
-      "source": {
-          "id": "google-news-in",
-          "name": "Google News (India)"
-      },
-      "author": "The Hindu",
-      "title": "Russia offers compensation and citizenship to kin of Indians killed in Ukraine war - The Hindu",
-      "description": "As Russian President Vladimir Putin accepted Prime Minister Narendra Modi’s request for early discharge of Indians hired as security helpers and forced to fight alongside the Russian Army against Ukraine, it has emerged that Moscow is offering a compensation package and citizenship to the families of the deceased.",
-      "url": "https://news.google.com/rss/articles/CBMijQFodHRwczovL3d3dy50aGVoaW5kdS5jb20vbmV3cy9uYXRpb25hbC9ydXNzaWEtb2ZmZXJzLWNvbXBlbnNhdGlvbi1hbmQtY2l0aXplbnNoaXAtdG8ta2luLW9mLWluZGlhbnMta2lsbGVkLWluLXVrcmFpbmUtd2FyL2FydGljbGU2ODM4NTE0Ni5lY2XSAZIBaHR0cHM6Ly93d3cudGhlaGluZHUuY29tL25ld3MvbmF0aW9uYWwvcnVzc2lhLW9mZmVycy1jb21wZW5zYXRpb24tYW5kLWNpdGl6ZW5zaGlwLXRvLWtpbi1vZi1pbmRpYW5zLWtpbGxlZC1pbi11a3JhaW5lLXdhci9hcnRpY2xlNjgzODUxNDYuZWNlL2FtcC8?oc=5",
-      "urlToImage": "https://static01.nyt.com/images/2022/03/16/us/politics/16dc-wardead-1/merlin_203726646_845a090f-8ed5-42a5-a179-f57056505fd3-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-      "publishedAt": "2024-07-09T16:26:00+00:00",
-      "content": null
-  },
-  {
-      "source": {
-          "id": "wired",
-          "name": "Wired"
-      },
-      "author": "Wired",
-      "title": "This Ancient Technology Is Helping Millions Stay Cool",
-      "description": "Cheap, low-energy evaporative cooling devices are keeping water, food, people, and even whole buildings cool across India.",
-      "url": "https://www.wired.com/story/evaporative-cooling-devices-coolant-clay-matka-mitticool-india-heat-wave/",
-      "urlToImage": "https://media.wired.com/photos/6672c421e0704c563b4e7b77/191:100/w_1280,c_limit/GettyImages-80973767.jpg",
-      "publishedAt": "2024-07-09T15:42:42Z",
-      "content": "CBalance is another company working to tackle the heat problem, specifically for underprivileged households living in tightly packed slums, ghettos, and poorly planned neighborhoods. Led by Vivek Gil… [+3227 chars]"
-  }
-  ]
+  articles =[]
+  //   {
+  //     "source": {
+  //         "id": "abc-news-au",
+  //         "name": "ABC News (AU)"
+  //     },
+  //     "author": "ABC News",
+  //     "title": "Indian Prime Minister Narendra Modi meets Vladimir Putin in Russia, condemns deaths of children in war and calls for peace",
+  //     "description": "Narendra Modi's pointed remarks came just moments after he was welcomed to the Kremlin by Vladimir Putin.",
+  //     "url": "https://www.abc.net.au/news/2024-07-10/narendra-modi-discusses-ukraine-with-vladimir-putin-in-russia/104077902",
+  //     "urlToImage": "https://live-production.wcms.abc-cdn.net.au/28f964e516f1663c43caa3836fd21f49?impolicy=wcms_watermark_news&cropH=2074&cropW=3687&xPos=0&yPos=39&width=862&height=485&imformat=generic",
+  //     "publishedAt": "2024-07-09T19:09:50Z",
+  //     "content": "Indian Prime Minister Narendra Modi has told Russian President Vladimir Putin that the death of innocent children was painful and terrifying, a day after a lethal strike on Kyiv's main hospital for c… [+5158 chars]"
+  // },
+  // {
+  //     "source": {
+  //         "id": "google-news-in",
+  //         "name": "Google News (India)"
+  //     },
+  //     "author": "The Times of India",
+  //     "title": "Mumbai BMW hit-and-run case: Juhu bar's license suspended for serving liquor to 23-year-old accused Mihir - The Times of India",
+  //     "description": "The state excise administration has suspended the license of a Juhu-Tara Road bar that served hard liquor to Mihir Shah, who had not yet attained the age of 25 years. This is a basic criterion for serving or drinking",
+  //     "url": "https://news.google.com/rss/articles/CBMisAFodHRwczovL3RpbWVzb2ZpbmRpYS5pbmRpYXRpbWVzLmNvbS9pbmRpYS9tdW1iYWktYm13LWhpdC1hbmQtcnVuLWNhc2UtanVodS1iYXJzLWxpY2Vuc2Utc3VzcGVuZGVkLWZvci1zZXJ2aW5nLWxpcXVvci10by0yMy15ZWFyLW9sZC1hY2N1c2VkLW1paGlyLXNoYWgvYXJ0aWNsZXNob3cvMTExNjEzNzgwLmNtc9IBtAFodHRwczovL3RpbWVzb2ZpbmRpYS5pbmRpYXRpbWVzLmNvbS9pbmRpYS9tdW1iYWktYm13LWhpdC1hbmQtcnVuLWNhc2UtanVodS1iYXJzLWxpY2Vuc2Utc3VzcGVuZGVkLWZvci1zZXJ2aW5nLWxpcXVvci10by0yMy15ZWFyLW9sZC1hY2N1c2VkLW1paGlyLXNoYWgvYW1wX2FydGljbGVzaG93LzExMTYxMzc4MC5jbXM?oc=5",
+  //     "urlToImage": "https://miller-ogorchock.com/wp-content/uploads/2016/10/hit-and-run.jpg",
+  //     "publishedAt": "2024-07-09T17:06:00+00:00",
+  //     "content": null
+  // },
+  // {
+  //     "source": {
+  //         "id": "google-news-in",
+  //         "name": "Google News (India)"
+  //     },
+  //     "author": "The Hindu",
+  //     "title": "Russia offers compensation and citizenship to kin of Indians killed in Ukraine war - The Hindu",
+  //     "description": "As Russian President Vladimir Putin accepted Prime Minister Narendra Modi’s request for early discharge of Indians hired as security helpers and forced to fight alongside the Russian Army against Ukraine, it has emerged that Moscow is offering a compensation package and citizenship to the families of the deceased.",
+  //     "url": "https://news.google.com/rss/articles/CBMijQFodHRwczovL3d3dy50aGVoaW5kdS5jb20vbmV3cy9uYXRpb25hbC9ydXNzaWEtb2ZmZXJzLWNvbXBlbnNhdGlvbi1hbmQtY2l0aXplbnNoaXAtdG8ta2luLW9mLWluZGlhbnMta2lsbGVkLWluLXVrcmFpbmUtd2FyL2FydGljbGU2ODM4NTE0Ni5lY2XSAZIBaHR0cHM6Ly93d3cudGhlaGluZHUuY29tL25ld3MvbmF0aW9uYWwvcnVzc2lhLW9mZmVycy1jb21wZW5zYXRpb24tYW5kLWNpdGl6ZW5zaGlwLXRvLWtpbi1vZi1pbmRpYW5zLWtpbGxlZC1pbi11a3JhaW5lLXdhci9hcnRpY2xlNjgzODUxNDYuZWNlL2FtcC8?oc=5",
+  //     "urlToImage": "https://static01.nyt.com/images/2022/03/16/us/politics/16dc-wardead-1/merlin_203726646_845a090f-8ed5-42a5-a179-f57056505fd3-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+  //     "publishedAt": "2024-07-09T16:26:00+00:00",
+  //     "content": null
+  // },
+  // {
+  //     "source": {
+  //         "id": "wired",
+  //         "name": "Wired"
+  //     },
+  //     "author": "Wired",
+  //     "title": "This Ancient Technology Is Helping Millions Stay Cool",
+  //     "description": "Cheap, low-energy evaporative cooling devices are keeping water, food, people, and even whole buildings cool across India.",
+  //     "url": "https://www.wired.com/story/evaporative-cooling-devices-coolant-clay-matka-mitticool-india-heat-wave/",
+  //     "urlToImage": "https://media.wired.com/photos/6672c421e0704c563b4e7b77/191:100/w_1280,c_limit/GettyImages-80973767.jpg",
+  //     "publishedAt": "2024-07-09T15:42:42Z",
+  //     "content": "CBalance is another company working to tackle the heat problem, specifically for underprivileged households living in tightly packed slums, ghettos, and poorly planned neighborhoods. Led by Vivek Gil… [+3227 chars]"
+  // }
+  // ]
   constructor(){
     super();
     console.log("Hello I am constructor from news component");
@@ -66,7 +66,30 @@ export class News extends Component {
       
     }
   }
+//   async componentDidMount() {
+//     //console.log("didmount");
+//     let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9817cbbb2d154748be294fc684545db0";
+//     try {
+//         let response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         let parsedData = await response.json();
+//         console.log(parsedData);
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//     }
+// }
+  async componentDidMount(){
+    console.log("didmount");
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=9817cbbb2d154748be294fc684545db0";
+    let data= await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+  } 
   render() {
+    console.log("render")
     return (
       <div className='container my-3'>
         <h2>Khabri-top headlines</h2>
@@ -74,7 +97,7 @@ export class News extends Component {
           <div className="row">
           {this.state.articles.map((element)=>{
              return <div className="col-md-4 " key={element.url} >
-             <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} ImageUrl={element.urlToImage} newsurl={element.url}/>
+             <NewsItem title={element.title ?element.title.slice(0, 45) :""} description={element.description ?element.description.slice(0, 88):""} ImageUrl={element.urlToImage} newsurl={element.url}/>
              </div>
           })}  
         </div>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
         
       <div className='my-3'>
         <div className="card" style={{width:"18rem"}}>
-            <img src={ImageUrl} className="card-img-top" alt="..."/>
+            <img src={!ImageUrl?"https://th.bing.com/th/id/OIP.VWChxFEpEc14eZmHhy9yowHaFj?w=1280&h=960&rs=1&pid=ImgDetMain":ImageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
